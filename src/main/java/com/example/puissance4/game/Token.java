1a15fa1b5adfd424291fe7345bd9249f08946c31
Yasser1080 <95435981+Yasser1080@users.noreply.github.com>
2024-05-07 11:33:53 +0200

    color

--- src/main/java/com/example/puissance4/game/Token.java
+++ src/main/java/com/example/puissance4/game/Token.java
@@ -3,12 +3,12 @@ package com.example.puissance4.game;
 public enum Token {
     Red{
         public String toString(){
-            return "R";
+            return "\u001B[31m" + "R" + "\u001B[0m";
         }
     },
     Yellow{
         public String toString(){
-            return "Y";
+            return "\u001B[33m" + "Y" + "\u001B[0m";
         }
     },
     Empty {
